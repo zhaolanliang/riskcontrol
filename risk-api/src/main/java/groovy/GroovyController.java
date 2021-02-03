287e0236f0a4d5dbaab4c277ba29163bb89985b0
zhaolanliang <1310571943@qq.com>
2021-02-03 16:42:49 +0800

    初始化

---
+++ risk-api/src/main/java/groovy/GroovyController.java
@@ -0,0 +1,8 @@
+package groovy;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GroovyController {	
+
+}
---
+++ risk-api/target/classes/shell/GroovyShell_3_4.groovy
@@ -0,0 +1,7 @@
+package com.google.groovy.shell
+
+def cal(int a, int b){
+    return a+b
+}
+
+cal(3,4)
